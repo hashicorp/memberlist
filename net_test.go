38a17123ef668e581c7fee71b9ab9ba8dfd93edc
Armon Dadgar <armon.dadgar@gmail.com>
2013-09-14 13:45:44 +0200

    Test piggypacking

--- net_test.go
+++ net_test.go
@@ -282,5 +282,79 @@ func TestTCPPushPull(t *testing.T) {
 }
 
 func TestSendMsg_Piggyback(t *testing.T) {
-	// TODO: test sendMsg will piggy back
+	m := GetMemberlist(t)
+	defer m.Shutdown()
+
+	// Add a message to be broadcast
+	a := alive{10, "rand", []byte{127, 0, 0, 255}}
+	m.encodeAndBroadcast("rand", aliveMsg, &a)
+
+	var udp *net.UDPConn
+	for port := 60000; port < 61000; port++ {
+		udpAddr := fmt.Sprintf("127.0.0.1:%d", port)
+		udpLn, err := net.ListenPacket("udp", udpAddr)
+		if err == nil {
+			udp = udpLn.(*net.UDPConn)
+			break
+		}
+	}
+
+	// Encode a ping
+	ping := ping{SeqNo: 42}
+	buf, err := encode(pingMsg, ping)
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+
+	// Send
+	addr := &net.UDPAddr{IP: []byte{127, 0, 0, 1}, Port: m.config.UDPPort}
+	udp.WriteTo(buf.Bytes(), addr)
+
+	// Wait for response
+	go func() {
+		time.Sleep(time.Second)
+		panic("timeout")
+	}()
+
+	in := make([]byte, 1500)
+	n, _, err := udp.ReadFrom(in)
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+	in = in[0:n]
+
+	msgType := uint8(in[0])
+	if msgType != compoundMsg {
+		t.Fatalf("bad response %v", in)
+	}
+
+	// get the parts
+	trunc, parts, err := decodeCompoundMessage(in[1:])
+	if trunc != 0 {
+		t.Fatalf("unexpected truncation")
+	}
+	if len(parts) != 2 {
+		t.Fatalf("unexpected parts %v", parts)
+	}
+	if err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+
+	var ack ackResp
+	if err := decode(parts[0][1:], &ack); err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+
+	if ack.SeqNo != 42 {
+		t.Fatalf("bad sequence no")
+	}
+
+	var aliveout alive
+	if err := decode(parts[1][1:], &aliveout); err != nil {
+		t.Fatalf("unexpected err %s", err)
+	}
+
+	if aliveout.Node != "rand" || aliveout.Incarnation != 10 {
+		t.Fatalf("bad mesg")
+	}
 }
