ebb2a2286841f4142809b77b5ebc7e60c69bb53f
Armon Dadgar <armon.dadgar@gmail.com>
2013-09-17 11:06:19 +0200

    Adding an integ test

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 test: subnet
 	go test ./...
 
+integ: subnet
+	GOMAXPROCS=4 INTEG_TESTS=yes go test ./...
+
 subnet:
 	./test/setup_subnet.sh
 
@@ -8,4 +11,4 @@ cov:
 	gocov test github.com/hashicorp/memberlist | gocov-html > /tmp/coverage.html
 	open /tmp/coverage.html
 
-.PNONY: test cov
+.PNONY: test cov integ
---
+++ integ_test.go
@@ -0,0 +1,79 @@
+package memberlist
+
+import (
+	"os"
+	"testing"
+	"time"
+)
+
+// CheckInteg will skip a test if integration testing is not enabled.
+func CheckInteg(t *testing.T) {
+	if !IsInteg() {
+		t.SkipNow()
+	}
+}
+
+// IsInteg returns a boolean telling you if we're in integ testing mode.
+func IsInteg() bool {
+	return os.Getenv("INTEG_TESTS") != ""
+}
+
+// Tests the memberlist by creating a cluster of 100 nodes
+// and checking that we get strong convergence of changes.
+func TestMemberlist_Integ(t *testing.T) {
+	CheckInteg(t)
+
+	num := 16
+	var members []*Memberlist
+
+	ch := make(chan *Node, num)
+	leaveCh := make(chan *Node, num)
+
+	for i := 0; i < num; i++ {
+		addr, _ := GetBindAddr()
+		c := DefaultConfig()
+		c.Name = addr
+		c.BindAddr = addr
+		c.RTT = 100 * time.Microsecond
+		c.ProbeInterval = 5 * time.Millisecond
+		c.GossipInterval = 3 * time.Millisecond
+		c.PushPullInterval = 20 * time.Millisecond
+
+		if i == 0 {
+			m, err := Create(c)
+			if err != nil {
+				t.Fatalf("unexpected err: %s", err)
+			}
+			members = append(members, m)
+			defer m.Shutdown()
+			m.NotifyJoin(ch)
+			m.NotifyLeave(leaveCh)
+		} else {
+			last := members[i-1]
+			m, err := Join(c, []string{last.config.Name})
+			if err != nil {
+				t.Fatalf("unexpected err: %s", err)
+			}
+			members = append(members, m)
+			defer m.Shutdown()
+		}
+	}
+
+	// Wait for node 1 to see all the others
+	for i := 1; i < num; i++ {
+		select {
+		case <-ch:
+		case l := <-leaveCh:
+			t.Fatalf("unexpected leave %s", l)
+		case <-time.After(10 * time.Millisecond):
+			t.Fatalf("timeout")
+		}
+	}
+	time.Sleep(10 * time.Millisecond)
+
+	for idx, m := range members {
+		if len(m.Members()) != num {
+			t.Fatalf("bad num %d at idx %d", len(m.Members()), idx)
+		}
+	}
+}
