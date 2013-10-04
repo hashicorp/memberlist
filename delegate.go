7023d983ccb732abb590e563114cac594f3f91ea
Armon Dadgar <armon.dadgar@gmail.com>
2013-10-04 16:34:29 -0700

    ChannelEventDelegate multiplexes a single channel to preserve order

--- delegate.go
+++ delegate.go
@@ -1,23 +1,36 @@
 package memberlist
 
+// Constant event types
+type NodeEventType int
+
+const (
+	NodeJoin NodeEventType = iota
+	NodeLeave
+)
+
 // ChannelEventDelegate is used to enable an application to receive
 // events about joins and leaves over a channel instead of a direct
 // function call
 type ChannelEventDelegate struct {
-	JoinCh  chan<- *Node
-	LeaveCh chan<- *Node
+	Ch chan<- NodeEvent
+}
+
+// NodeEvent is used to represent a node event
+type NodeEvent struct {
+	Event NodeEventType
+	Node  *Node
 }
 
 func (c *ChannelEventDelegate) NotifyJoin(n *Node) {
 	select {
-	case c.JoinCh <- n:
+	case c.Ch <- NodeEvent{NodeJoin, n}:
 	default:
 	}
 }
 
 func (c *ChannelEventDelegate) NotifyLeave(n *Node) {
 	select {
-	case c.LeaveCh <- n:
+	case c.Ch <- NodeEvent{NodeLeave, n}:
 	default:
 	}
 }
