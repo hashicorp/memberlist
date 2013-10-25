ac6c607ce4b23cff1ee5de4c036e62dcd355bf30
Armon Dadgar <armon.dadgar@gmail.com>
2013-10-24 21:58:50 -0700

    Adding various crypto functions

---
+++ security.go
@@ -0,0 +1,147 @@
+package memberlist
+
+import (
+	"bytes"
+	"code.google.com/p/go.crypto/pbkdf2"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha1"
+	"fmt"
+	"io"
+)
+
+const (
+	KeySalt    = "\xb1\x94\x18k$\x9cc\xb4++of\x8e\xcd\x8c\x84\xf7\xf6F:\xd6d\x1e\x15\x82Mj\xd5~M\xa5<"
+	KeyRounds  = 2048
+	KeyLength  = aes.BlockSize
+	HMACLength = sha1.Size
+)
+
+// deriveKey is used to generat the encryption key we use from the secret
+// that is provided. We use PBKDF2 to ensure the key is crypto worthy
+func deriveKey(secret []byte) []byte {
+	return pbkdf2.Key(secret, []byte(KeySalt), KeyRounds, KeyLength, sha1.New)
+}
+
+// pkcs7encode is used to pad a byte buffer to a specific block size using
+// the PKCS7 algorithm. "Ignores" some bytes to compensate for IV
+func pkcs7encode(buf *bytes.Buffer, ignore, blockSize int) {
+	n := buf.Len() - ignore
+	more := blockSize - (n % blockSize)
+	if more == blockSize {
+		return
+	}
+	for i := 0; i < more; i++ {
+		buf.WriteByte(byte(more))
+	}
+}
+
+// pkcs7decode is used to decode a buffer that has been padded
+func pkcs7decode(buf []byte, blockSize int) []byte {
+	if len(buf) == 0 {
+		panic("Cannot decode a PKCS7 buffer of zero length")
+	}
+	n := len(buf)
+	last := buf[n-1]
+	n -= (int(last) % blockSize)
+	return buf[:n]
+}
+
+// encryptPayload is used to encrypt a message with a given key.
+// We make use of AES-128 in CBC mode. New byte buffer is the
+// IV, and encrypted text, aligned to 16byte block size
+func encryptPayload(key []byte, msg []byte) (*bytes.Buffer, error) {
+	// Create a buffer
+	buf := bytes.NewBuffer(nil)
+
+	// Add a random IV
+	io.CopyN(buf, rand.Reader, aes.BlockSize)
+
+	// Copy the message
+	io.Copy(buf, bytes.NewReader(msg))
+
+	// Ensure we are correctly padded
+	pkcs7encode(buf, aes.BlockSize, aes.BlockSize)
+
+	// Get the AES block cipher
+	aesBlock, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	// Encrypt message using CBC
+	slice := buf.Bytes()
+	cbc := cipher.NewCBCEncrypter(aesBlock, slice[:aes.BlockSize])
+	cbc.CryptBlocks(slice[aes.BlockSize:], slice[aes.BlockSize:])
+	return buf, nil
+}
+
+// decryptPayload is used to decrypt a message with a given key.
+// Uses same algorithm as encryptPayload. Any padding will be
+// removed, and a new slice is returned. Decryption is done
+// IN PLACE!
+func decryptPayload(key []byte, msg []byte) ([]byte, error) {
+	// Get the AES block cipher
+	aesBlock, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	// Decrypt using CBC mode
+	cbc := cipher.NewCBCDecrypter(aesBlock, msg[:aes.BlockSize])
+	cbc.CryptBlocks(msg[aes.BlockSize:], msg[aes.BlockSize:])
+
+	// Remove any padding
+	noPad := pkcs7decode(msg[aes.BlockSize:], aes.BlockSize)
+	return noPad, err
+}
+
+// hmacPayload is used to append an HMAC-SHA1 to a payload
+// after computing the value using a given key
+func hmacPayload(key []byte, msg *bytes.Buffer) error {
+	// Create the HMAC
+	mac := hmac.New(sha1.New, key)
+
+	// Feed in the data
+	_, err := mac.Write(msg.Bytes())
+	if err != nil {
+		return err
+	}
+
+	// Compute the hmac
+	hmacSum := mac.Sum(nil)
+
+	// Append the hmac to the bytes
+	msg.Write(hmacSum)
+	return nil
+}
+
+// hmacVerifyPayload is used to verify the HMAC of a payload.
+// It uses the last HMACLength bytes as the provided HMAC, and computes
+// the HMAC of the preceeding bytes.
+func hmacVerifyPayload(key []byte, buf []byte) error {
+	if len(buf) <= HMACLength {
+		return fmt.Errorf("Buffer is too short for HMAC verification")
+	}
+
+	// Extract the hmac, and the message
+	n := len(buf)
+	providedHMAC := buf[n-HMACLength:]
+	msg := buf[:n-HMACLength]
+
+	// Compute the HMAC
+	mac := hmac.New(sha1.New, key)
+	_, err := mac.Write(msg)
+	if err != nil {
+		return err
+	}
+	hmacSum := mac.Sum(nil)
+
+	// Verify equality
+	if !hmac.Equal(providedHMAC, hmacSum) {
+		return fmt.Errorf("HMAC verification failed")
+	}
+	return nil
+}
---
+++ security_test.go
@@ -0,0 +1,108 @@
+package memberlist
+
+import (
+	"bytes"
+	"crypto/aes"
+	"reflect"
+	"testing"
+)
+
+func TestDeriveKey(t *testing.T) {
+	k1 := deriveKey([]byte("foobar"))
+	k2 := deriveKey([]byte("foobar"))
+	if bytes.Compare(k1, k2) != 0 {
+		t.Fatalf("not equal")
+	}
+
+	k3 := deriveKey([]byte("test"))
+	if bytes.Compare(k1, k3) == 0 {
+		t.Fatalf("should not be equal")
+	}
+
+	if len(k1) != aes.BlockSize {
+		t.Fatalf("bad len: %d", len(k1))
+	}
+	if len(k3) != aes.BlockSize {
+		t.Fatalf("bad len: %d", len(k1))
+	}
+}
+
+func TestPKCS7(t *testing.T) {
+	for i := 1; i <= 32; i++ {
+		// Make a buffer of size i
+		buf := []byte{}
+		for j := 0; j < i; j++ {
+			buf = append(buf, byte(i))
+		}
+
+		// Copy to bytes buffer
+		inp := bytes.NewBuffer(nil)
+		inp.Write(buf)
+
+		// Pad this out
+		pkcs7encode(inp, 0, 16)
+
+		// Unpad
+		dec := pkcs7decode(inp.Bytes(), 16)
+
+		// Ensure equivilence
+		if !reflect.DeepEqual(buf, dec) {
+			t.Fatalf("mismatch: %v %v", buf, dec)
+		}
+	}
+
+}
+
+func TestEncryptDecrypt(t *testing.T) {
+	k1 := deriveKey([]byte("foobar"))
+	plaintext := []byte("this is a plain text message")
+
+	buf, err := encryptPayload(k1, plaintext)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	if buf.Len()%16 != 0 {
+		t.Fatalf("output should be 16 byte aligned")
+	}
+
+	msg, err := decryptPayload(k1, buf.Bytes())
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	if bytes.Compare(msg, plaintext) != 0 {
+		t.Fatalf("encrypt/decrypt failed! %v", msg)
+	}
+}
+
+func TestHMACVerify(t *testing.T) {
+	k1 := deriveKey([]byte("foobar"))
+	plaintext := []byte("this is a plain text message")
+
+	buf := bytes.NewBuffer(nil)
+	buf.Write(plaintext)
+
+	if err := hmacPayload(k1, buf); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	if err := hmacVerifyPayload(k1, buf.Bytes()); err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Try different key!
+	k1[0]++
+	err := hmacVerifyPayload(k1, buf.Bytes())
+	if err.Error() != "HMAC verification failed" {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Try a different payload
+	k1[0]--
+	buf.Bytes()[0]++
+	err = hmacVerifyPayload(k1, buf.Bytes())
+	if err.Error() != "HMAC verification failed" {
+		t.Fatalf("err: %v", err)
+	}
+}
