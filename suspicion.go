26206f575fcd9188584a4997b4330ef1e498f838
James Phillips <james@hashicorp.com>
2016-05-26 21:53:28 -0700

    Adds documentation for the suspicion class.

--- suspicion.go
+++ suspicion.go
@@ -5,15 +5,37 @@ import (
 	"time"
 )
 
+// suspicion manages the suspect timer for a node and provides an interface
+// to accelerate the timeout as we get more independent confirmations that
+// a node is suspect.
 type suspicion struct {
-	k             float64
-	min           float64
-	max           float64
-	start         time.Time
-	timer         *time.Timer
+	// k is the number of independent confirmations we'd like to see in
+	// order to drive the timer to its minimum value. This is a float so
+	// we don't have to cast it later.
+	k float64
+
+	// min is the minimum timer value in seconds.
+	min float64
+
+	// max is the maximum timer value in seconds.
+	max float64
+
+	// start captures the timestamp when we began the timer. This is used
+	// so we can calculate durations to feed the timer during updates in
+	// a way the achieves the overall time we'd like.
+	start time.Time
+
+	// timer is the underlying timer that implements the timeout.
+	timer *time.Timer
+
+	// confirmations is a map of "from" nodes that have confirmed a given
+	// node is suspect. This prevents double counting, and also serves as
+	// our source of truth for counting confirmations.
 	confirmations map[string]struct{}
 }
 
+// newSuspicion returns a timer started with the max time, and that will drive
+// to the min time after seeing k or more confirmations.
 func newSuspicion(k int, min time.Duration, max time.Duration, f func()) *suspicion {
 	return &suspicion{
 		k:             float64(k),
@@ -25,6 +47,8 @@ func newSuspicion(k int, min time.Duration, max time.Duration, f func()) *suspic
 	}
 }
 
+// Corroborate registers that a possibly new peer has also determined the given
+// node is suspect.
 func (s *suspicion) Corroborate(from string) {
 	// Only allow one confirmation from each possible peer.
 	if _, ok := s.confirmations[from]; ok {
