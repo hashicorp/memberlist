603c9458f35f9e26e91de2ffbcaa83c45d68eadb
Sergey Kacheev <s.kacheev@gmail.com>
2016-09-23 22:34:29 +0300

    Expect addr like ::1 in hasPort util

--- util.go
+++ util.go
@@ -327,10 +327,19 @@ func isLoopbackIP(ip_str string) bool {
 	return loopbackBlock.Contains(ip)
 }
 
-// Given a string of the form "host", "host:port", or "[ipv6::address]:port",
+// Given a string of the form "host", "host:port",
+// "ipv6::addr" or "[ipv6::address]:port",
 // return true if the string includes a port.
 func hasPort(s string) bool {
-	return strings.LastIndex(s, ":") > strings.LastIndex(s, "]")
+	if s == "" {
+		return false
+	}
+	last := strings.LastIndex(s, ":")
+	if s[0] == '[' {
+		return last > strings.LastIndex(s, "]")
+	}
+	first := strings.Index(s, ":")
+	return first != -1 && first == last
 }
 
 // compressPayload takes an opaque input buffer, compresses it
